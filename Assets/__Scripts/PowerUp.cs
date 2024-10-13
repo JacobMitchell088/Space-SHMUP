5f0bc2444f0e6f35254ccc6ddeb8c24eb076beca
JacobMitchell088 <jacob.mitchell088@gmail.com>
2024-10-13 01:03:00 -0500

    power up floating

--- Assets/__Scripts/PowerUp.cs
+++ Assets/__Scripts/PowerUp.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 [RequireComponent(typeof(BoundsCheck))]
@@ -17,7 +18,7 @@ public class PowerUp : MonoBehaviour
     [Header("Dynamic")]
     public eWeaponType _type;
     public GameObject cube;
-    public TextMesh letter;
+    public TextMeshPro letter;
     public Vector3 rotPerSecond;
     public float birthTime;
     private Rigidbody rigid;
@@ -26,7 +27,7 @@ public class PowerUp : MonoBehaviour
 
     void Awake() {
         cube = transform.GetChild(0).gameObject;
-        letter = GetComponent<TextMesh>();
+        letter = GetComponent<TextMeshPro>();
         rigid = GetComponent<Rigidbody>();
         bndCheck = GetComponent<BoundsCheck>();
         cubeMat = cube.GetComponent<Renderer>().material;
